c1ef2abc86d0fc8d29083a7cd55be7c65d939bad
rizkygm23 <rizky230504@gmail.com>
2024-07-27 22:38:00 +0700

    All done || Next Costumize md + screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.{html,js}'],
+  content: ['./public/**/*.{html,js}', '.src/css/*.{css}'],
+  
   theme: {
 
     extend: {
       keyframes: {
-        float: {
+        'float': {
           '0%, 100%': { transform: 'translateY(0)' },
           '50%': { transform: 'translateY(-10px)' },
+          
         },
+        'border-spin': {
+          '100%' : {
+            transform: 'rotate(-360deg)',
+          }
+
+        },
+
       },
       animation: {
-        float: 'float 3s ease-in-out infinite',
+        'float': 'float 3s ease-in-out infinite',
+        'border-spin': 'border-spin 7s linear infinite',
       },
     },
   },
